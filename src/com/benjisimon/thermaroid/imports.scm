550e318e8949233139ce0af22dff7196f845f3ee
Ben Simon <ben@ideas2executables.com>
2015-11-18 10:55:48 -0500

    focus on press now

--- src/com/benjisimon/thermaroid/imports.scm
+++ src/com/benjisimon/thermaroid/imports.scm
@@ -3,15 +3,17 @@
 ;;
 
 (define-alias Activity android.app.Activity)
+(define-alias Camera android.hardware.Camera)
+(define-alias CameraAutoFocusCallback android.hardware.Camera$AutoFocusCallback)
 (define-alias CameraCharacteristics android.hardware.camera2.CameraCharacteristics)
 (define-alias CameraDevice android.hardware.camera2.CameraDevice)
 (define-alias CameraDeviceStateCallback android.hardware.camera2.CameraDevice$StateCallback)
 (define-alias CameraManager android.hardware.camera2.CameraManager)
 (define-alias Context android.content.Context)
 (define-alias Log android.util.Log)
+(define-alias MotionEvent android.view.MotionEvent)
 (define-alias SurfaceTexture android.graphics.SurfaceTexture)
 (define-alias TextureView android.view.TextureView)
 (define-alias TextureViewListener android.view.TextureView$SurfaceTextureListener)
-(define-alias Camera android.hardware.Camera)
-(define-alias CameraAutoFocusCallback android.hardware.Camera$AutoFocusCallback)
+(define-alias ViewOnTouchListener android.view.View$OnTouchListener)
 
--- src/com/benjisimon/thermaroid/main.scm
+++ src/com/benjisimon/thermaroid/main.scm
@@ -11,8 +11,12 @@
 (activity main
           (on-create-view
            (let* ((texture-view (TextureView (this)))
-                  (camera :: Camera #!null))
-             (logi "Hardware accelerated? " (texture-view:is-hardware-accelerated))
+                  (camera :: Camera #!null)
+                  (on-focus :: CameraAutoFocusCallback 
+                            (object (CameraAutoFocusCallback)
+                                    ((on-auto-focus (success :: boolean) (camera :: Camera)) :: void
+                                     (logi "Focused? " success)))))
+           (logi "Hardware accelerated? " (texture-view:is-hardware-accelerated))
              (texture-view:set-surface-texture-listener 
               (object (TextureViewListener)
                       ((on-surface-texture-available (surface :: SurfaceTexture)
@@ -22,13 +26,10 @@
                          (set! camera c)
                          (camera:setPreviewTexture surface)
                          (camera:startPreview)
-                         (camera:autoFocus (object (CameraAutoFocusCallback)
-                                                   ((on-auto-focus (success :: boolean) (camera :: Camera)) :: void
-                                                    (logi "Focused? " success))))))
-
-
+                         (camera:auto-focus on-focus)))
+                      
                       ((on-surface-texture-size-changed (surface :: SurfaceTexture)
-                                                        (width :: int)
+                                                         (width :: int)
                                                         (height :: int))
                        #!void)
 
@@ -38,5 +39,11 @@
                        #t)
                       ((on-surface-texture-updated (surface :: SurfaceTexture))
                        #!void)))
+             (texture-view:set-on-touch-listener (object (ViewOnTouchListener)
+                                                         ((on-touch (view :: View) (evt :: MotionEvent)) :: boolean
+                                                          (if (equal? MotionEvent:ACTION_UP  (evt:get-action))
+                                                            (camera:auto-focus on-focus)
+                                                            (logi "Taking photo"))
+                                                          #t)))
              texture-view)))
 
